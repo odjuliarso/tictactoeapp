6afd73d9ccb7dea90e929ba42f518d6e97aa18a4
Othman Djuliarso <37389043+odjuliarso@users.noreply.github.com>
2023-05-18 19:01:28 -0700

    update set cell method to only set when cells are blank

--- TicTacToeApp/Data/Model/TicModel.swift
+++ TicTacToeApp/Data/Model/TicModel.swift
@@ -33,6 +33,9 @@ struct TicModel {
         guard _grid.indices.contains(n) else {
             return
         }
+        guard _grid[n] == .b else {
+            return
+        }
         _grid[n] = c
     }
 }
